22aeb9bcf294692242ed8f30c669f5718cdf8cb2
joshuadelarosa1 <118570698+joshuadelarosa1@users.noreply.github.com>
2023-11-13 23:23:56 -0600

    initial commit pt2

---
+++ src/BitTree.java
@@ -0,0 +1,41 @@
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+public class BitTree {
+
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  int n;
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  public BitTree(int n) {
+    // STUB
+  } // BitTree(int n)
+
+  // +----------------+----------------------------------------------
+  // | Public Methods |
+  // +----------------+
+
+  public void set(String bits, String value) {
+    // STUB
+  } // set(String bits, String value)
+
+  public String get(String bits) {
+    // STUB
+    return "";
+  } // get(String bits)
+
+  public void dump(PrintWriter pen) {
+    // STUB
+  } // dump(PrintWriter pen)
+
+  public void load(InputStream source) {
+    // STUB
+  } // load(InputStream source)
+
+} // class BitTree
---
+++ src/BrailleASCIITables.java
@@ -0,0 +1,22 @@
+public class BrailleASCIITables {
+
+  // +----------------+----------------------------------------------
+  // | Public Methods |
+  // +----------------+
+
+  public static String toBraille(char letter) {
+    return "";
+    // STUB
+  } // toBraille(char letter)
+
+  public static String toASCII(String bits) {
+    return "";
+    // STUB
+  } // toASCII(String bits)
+
+  public static String toUnicode(String bits) {
+    return "";
+    // STUB
+  } // toUnicode(String bits)
+
+} // class BrailleASCIITables
